b3a00095b0f803b740332c162b31c7def6ac23a8
adi stef <adrian_2354@yahoo.com>
2018-04-04 13:14:12 +0300

    6 completed challanges

--- js/res/main.js
+++ js/res/main.js
@@ -2,12 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // solveMultiples();
     // solveFibonacci();
     // solvePrimeFactor();
-
-    
+    // solvePalindromProduct();
+    // console.time("adi");
+    // solveSmallestMultiple();
+    // console.timeEnd("adi");
+    // solveSumSquareDifference();
+    // solve10001Prime();
 });
-
-
-
 
 function solveMultiples() {
     var sum=0;
@@ -63,4 +64,74 @@ function solvePrimeFactor() {
     while(number>=currentNr);
 
     return("Largest prime factor of given number is: ", primeFactorArray[primeFactorArray.length-1]);
+}
+
+function solvePalindromProduct() {
+    let maxNr = 1;
+    for (let i = 999; i>=1; i--) {
+        for (let j = 999; j>=1;j--) {
+            let product = i * j;
+            let productString = product.toString();
+            let productStringReversed = productString.split("").reverse().join("");
+
+            if (productString === productStringReversed) {
+                if (product > maxNr) {
+                    maxNr = product;
+                }
+            }
+        }
+    }
+    return("Largest palindrome product: ", maxNr);
+}
+
+function solveSmallestMultiple() {
+    let smallestMultiple = 1,
+        sum = 1,
+        flag = true;
+
+    do {
+        for (let i=1;i<=20;i++) {
+            sum += (Math.floor(smallestMultiple % i));
+        }
+        if (sum == 1){
+            flag = false;
+        }
+        else
+            smallestMultiple++;
+            sum = 1;
+    }
+    while (flag);
+    return("Smallest multiple is: ",smallestMultiple);
+}
+
+function solveSumSquareDifference() {
+    let sumOfSquares = 0,
+        squareOfSum  = 0;
+
+    for (let i=1; i<=100; i++) {
+        sumOfSquares += i*i;
+        squareOfSum += i;
+    }
+
+    squareOfSum = squareOfSum * squareOfSum;
+    return("Sum square difference is: ",squareOfSum - sumOfSquares);    
+}
+
+function solve10001Prime() {
+    // Math.sqrt(9);
+    // do not check division by 2
+    let array = [2];
+    let flag = true;
+    let number = 3;
+
+    if (array.length <= 10001) {
+        for (let i=Math.floor(Math.sqrt(number)); i>=1;i--) {
+            if (number % i !== 0) {
+
+            }
+        }
+    }
+    else
+        console.log(array[array.length-1]);
+
 }
